9c422c1df7cdfb23449748b57b58f783be47e22d
Philip Dimaano <pdimaano7@gmail.com>
2022-11-08 12:32:48 -0800

    Refactored with authenticate method, ran successful tests

--- routes/auth.js
+++ routes/auth.js
@@ -8,21 +8,18 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const { SECRET_KEY } = require("../config");
 const { BadRequestError, UnauthorizedError } = require("../expressError");
-const User = require("../models/user");
 
 /** POST /login: {username, password} => {token} */
 
 router.post("/login", async function (req, res, next) {
   if (req.body === undefined) throw new BadRequestError();
   const { username, password } = req.body;
-  const isValidLogin = User.authenticate(username, password);
+  const isValidLogin = await User.authenticate(username, password);
 
   if (isValidLogin) {
-    if ((await bcrypt.compare(password, hashPassword.password)) === true) {
       const token = jwt.sign({ username }, SECRET_KEY);
       User.updateLoginTimestamp(username);
       return res.json({ token });
-    }
   }
   throw new UnauthorizedError("Invalid user/password");
 });
